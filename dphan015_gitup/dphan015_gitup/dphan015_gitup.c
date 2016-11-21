3102341e4f45fd6c5fecce072ff6938bd9d92b7b
David Phan <dphan015@ucr.edu>
2016-11-21 13:45:20 -0800

    IR sensor working

--- dphan015_gitup/dphan015_gitup/dphan015_gitup.c
+++ dphan015_gitup/dphan015_gitup/dphan015_gitup.c
@@ -16,39 +16,104 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "croutine.h"
+#include "usart_ATmega1284.h"
 
 // Create State Machine enumeration here
+enum led_states {INIT, OFF, ON} led_state;
+
+unsigned char tmpB;
+unsigned char led;
+unsigned char trigger;
 
 void InitFct(){
 	// Initialize State Machines here
+	led_state = INIT;
 }
 
 void TickFct(){
-	// Transitions
+	
+	tmpB = ~PINB & 0x01;
+	
+	switch (led_state) { // transitions
+		case INIT:
+		led_state = OFF;
+		break;
+		
+		case OFF:
+		if(tmpB == 1){
+			led_state = ON;
+		}
+		else{
+			led_state = OFF;
+		}
+		break;
+		
+		case ON:
+		if(tmpB == 1){
+			led_state = ON;
+		}
+		else{
+			led_state = OFF;
+		}
 
-	// Actions
+		break;
+		default:
+		
+		break;
+	}
+	
+	switch (led_state) { // Actions
+		case INIT:
+		led = 0x00;
+		trigger = 0x00;
+		break;
+		
+		case OFF:
+		led = 0x00;
+		break;
+		
+		case ON:
+		led = 0x01;
+		trigger = 0x01;
+		if(USART_IsSendReady(0)) {
+			USART_Send(trigger,0);
+		}
+		break;
+		
+		default:
+		break;
+	}
+	
+	PORTA = led;
+	
 }
 
 void TaskFct()
 {
-	// InitFct()
+	InitFct();
 	for(;;)
 	{
-		// TickFct()
-		vTaskDelay(100);
+		TickFct();
+		vTaskDelay(1);
 	}
 }
 
 void PulseFct(unsigned portBASE_TYPE Priority)
 {
 	// Create Tasks here
-	xTaskCreate(/*TaskFct*/, (signed portCHAR *)/*"TaskFct"*/, configMINIMAL_STACK_SIZE, NULL, Priority, NULL );
+	xTaskCreate(TaskFct, (signed portCHAR *)"TaskFct", configMINIMAL_STACK_SIZE, NULL, Priority, NULL );
 }
 
 int main(void)
 {
-	while(1)
-	{
-		// TODO: Please write your application code
-	}
+	DDRA = 0xFF;	PORTA = 0x00;	// LED output
+	DDRB = 0x00;	PORTB = 0xFF;	// IR input
+	
+	initUSART(0);
+	
+	PulseFct(1);
+	vTaskStartScheduler();
+	
+	
+	return 0;
 }
